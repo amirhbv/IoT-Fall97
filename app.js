3a58d0336e28b172373c41a9965c69f7b769d448
Mohsen Fayyaz <mohsen.fayyaz77@gmail.com>
2018-12-11 00:23:22 +0330

    panel added

--- app.js
+++ app.js
@@ -11,12 +11,14 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//app.use(express.cookieSession({ secret: 'secret', cookie: { maxAge: 60 * 60 * 1000 }}));
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 // routes setup
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Login'});
+  res.render('panel/index.html', { username: 'MohsenFayyaz'});
 });
 module.exports = router;
