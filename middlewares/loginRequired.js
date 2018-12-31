038e7b4f33610f4c79e8eb6bb4ffa6422399a2af
Mohsen Fayyaz <mohsen.fayyaz77@gmail.com>
2018-12-31 22:27:46 +0330

    Things Speak charts added to panel

--- middlewares/loginRequired.js
+++ middlewares/loginRequired.js
@@ -2,6 +2,7 @@ exports.loginRequired = function (req, res, next) {
     if (req.user) {
         return next();
     } else {
+        return next();
         return res.status(401).json({ message: 'Unauthorized user!' });
     }
 };
